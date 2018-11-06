b63a0acdefc6d0cb6fe3a42dabc218bec0e5a8c1
arturpmorais <43376579+arturpmorais@users.noreply.github.com>
2018-11-06 08:45:42 -0200

    tirei é nois

--- Programa/ProjetoClinica/ProjetoClinica/paciente/consultas/agenda.aspx.designer.cs
+++ Programa/ProjetoClinica/ProjetoClinica/paciente/consultas/agenda.aspx.designer.cs
@@ -11,14 +11,5 @@ namespace ProjetoClinica.paciente.consultas {
     
     
     public partial class agenda {
-        
-        /// <summary>
-        /// Controle Table1.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Table Table1;
     }
 }
